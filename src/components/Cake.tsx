d576e92b16f9959bdbf5cbc2687be94bf15e245b
sauce <dadad1004@naver.com>
2024-08-19 23:35:36 +0900

    chore: add candles to cake

--- src/components/Cake.tsx
+++ src/components/Cake.tsx
@@ -1,13 +1,21 @@
-import { cake, cakeImg, candle2 } from '@/styles/cake.css';
+import {
+  cake,
+  cakeImg,
+  candle1,
+  candle2,
+  candle3,
+  candle5,
+} from '@/styles/cake.css';
 import Image from 'next/image';
-import Candle from './Candle';
+import CakeCandle from './CakeCandle';
 
 export default function Cake() {
   return (
     <div className={cake}>
-      <div className={candle2}>
-        <Candle id={2} />
-      </div>
+      <CakeCandle id={4} candlePositionClass={candle3} />
+      <CakeCandle id={2} candlePositionClass={candle2} />
+      <CakeCandle id={3} candlePositionClass={candle1} />
+      <CakeCandle id={3} candlePositionClass={candle5} />
       <Image
         src={'/assets/cake.svg'}
         alt="cake"
