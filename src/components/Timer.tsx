7ca7ce9bfed23c5a0a78d406196e449b63e6640a
sauce <dadad1004@naver.com>
2024-08-28 22:14:30 +0900

    chore: Refactor Timer component for improved layout consistency and code organization

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,11 +2,16 @@
 
 import {
   timer,
-  timerContainer,
   timerContent,
   birthdayText,
   timerText,
   boxesImg,
+  timerComponentContainer,
+  clickableBoxesImg,
+  timerContainer,
+  textBalloon,
+  endedTimerComponentContainer,
+  boxesAndTimerContainer,
 } from '@/styles/timer.css';
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
@@ -16,7 +21,7 @@ import { useCallback, useEffect, useState } from 'react';
  * @param birthday birthday date
  * @returns Timer component
  */
-export default  function Timer({ birthday }: { birthday: Date }) {
+export default function Timer({ birthday }: { birthday: Date }) {
   // State for remains
   const [remains, setRemains] = useState(1);
 
@@ -29,6 +34,7 @@ export default  function Timer({ birthday }: { birthday: Date }) {
 
   // Update remains every second
   useEffect(() => {
+    calculateRemains();
     const interval = setInterval(() => {
       calculateRemains();
     }, 1000);
@@ -36,46 +42,63 @@ export default  function Timer({ birthday }: { birthday: Date }) {
   }, [calculateRemains]);
 
   return (
-    <div className={timerContainer}>
+    <div
+      className={
+        remains > 0 ? timerComponentContainer : endedTimerComponentContainer
+      }
+    >
+      {remains <= 0 && (
+        <div className={textBalloon}>
+          이제 친구들의 축하글을 열어볼 수 있어요!
+          <br />
+          편지는 “일주일”간 보관돼요.
+        </div>
+      )}
+      <div className={boxesAndTimerContainer}>
       <Image
         src={'/assets/boxes.svg'}
         alt="boxes"
         width={0}
         height={0}
-        className={boxesImg}
-      />
-      <div className={timer}>
-        <div className={timerContent}>
-          <div className={timerText}>
-            D-
-            {Math.floor(remains / (1000 * 60 * 60 * 24) + 0.5)}
-          </div>
-          <div>
-            {
+        className={remains > 0 ? boxesImg : clickableBoxesImg}
+        />
+      {remains > 0 && (
+        <div className={timerContainer}>
+          <div className={timer}>
+            <div className={timerContent}>
               <div className={timerText}>
-                {/* HH:MM:SS */}
-                {`${Math.floor(
-                  (remains % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-                )
-                  .toString()
-                  .padStart(2, '0')}:${Math.floor(
-                  (remains % (1000 * 60 * 60)) / (1000 * 60)
-                )
-                  .toString()
-                  .padStart(2, '0')}:${Math.floor(
-                  (remains % (1000 * 60)) / 1000
-                )
-                  .toString()
-                  .padStart(2, '0')}`}
+                D-
+                {Math.floor(remains / (1000 * 60 * 60 * 24) + 0.5)}
               </div>
-            }
+              <div>
+                {
+                  <div className={timerText}>
+                    {/* HH:MM:SS */}
+                    {`${Math.floor(
+                      (remains % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+                    )
+                    .toString()
+                    .padStart(2, '0')}:${Math.floor(
+                      (remains % (1000 * 60 * 60)) / (1000 * 60)
+                    )
+                    .toString()
+                    .padStart(2, '0')}:${Math.floor(
+                      (remains % (1000 * 60)) / 1000
+                    )
+                    .toString()
+                    .padStart(2, '0')}`}
+                  </div>
+                }
+              </div>
+            </div>
+          </div>
+          <div className={birthdayText}>
+            {`생일: ${birthday.getFullYear()}.${
+              birthday.getMonth() + 1
+            }.${birthday.getDate()}.`}
           </div>
         </div>
-      </div>
-      <div className={birthdayText}>
-        {`생일: ${birthday.getFullYear()}.${
-          birthday.getMonth() + 1
-        }.${birthday.getDate()}.`}
+      )}
       </div>
     </div>
   );
--- src/styles/timer.css.ts
+++ src/styles/timer.css.ts
@@ -1,43 +1,91 @@
 import { style } from '@vanilla-extract/css';
-import { flexCenterContainer } from './common/common.css';
+import { flexCenterContainer, uiyeun } from './common/common.css';
 
-export const timerContainer = style([
+export const timerComponentContainer = style([
+  flexCenterContainer,
   {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
     flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
     position: 'relative',
-    gap: '0.5rem',
-    padding: '2rem',
+    padding: '3rem 0',
     boxSizing: 'border-box',
   },
 ]);
+
+export const endedTimerComponentContainer = style([
+  timerComponentContainer,
+  {
+    flexDirection: 'column',
+    position: 'relative',
+    padding: '1rem 0 2rem 0',
+    boxSizing: 'border-box',
+  },
+]);
+
+export const boxesAndTimerContainer = style([
+  flexCenterContainer,
+  { flexDirection: 'column', position: 'relative' },
+]);
+
+export const timerContainer = style([
+  flexCenterContainer,
+  { flexDirection: 'column', gap: '0.5rem' },
+]);
+
+export const textBalloon = style({
+  display: 'block',
+  position: 'relative',
+  width: 'auto',
+  height: 'auto',
+  fontSize: '1rem',
+  textAlign: 'center',
+  backgroundColor: '#FFFFFFB2',
+  borderRadius: '6px',
+  padding: '0.5rem 1rem',
+  boxSizing: 'border-box',
+  marginBottom: '0.5rem',
+  fontFamily: uiyeun,
+  fontWeight: 400,
+  color: '#EA0000',
+
+  '::after': {
+    content: '""',
+    borderTop: '15px solid #FFFFFFB2',
+    borderLeft: '10px solid transparent',
+    borderRight: '10px solid transparent',
+    borderBottom: '0 solid transparent',
+    position: 'absolute',
+    bottom: '-15px',
+    left: 'calc(50% - 15px)',
+  },
+});
 
 export const boxesImg = style({
   display: 'block',
   position: 'absolute',
   width: '4rem',
   height: 'auto',
-  top: 0,
+  top: '-1.5rem',
 });
 
-export const timer = style(
-  {
-    display: 'flex',
-    flexDirection: 'row',
-    width: '100%',
-    height: '4.125rem',
-    maxWidth: '25rem',
-    boxSizing: 'border-box',
-    border: '3px dashed #FE0303',
-    borderRadius: '16px',
-    backgroundColor: '#FFFFFFB2',
-    boxShadow: '0px 4px 14px 0px #00000040',
-  },
-);
+export const clickableBoxesImg = style({
+  display: 'block',
+  width: '7rem',
+  height: 'auto',
+  cursor: 'pointer',
+});
+
+export const timer = style({
+  display: 'flex',
+  flexDirection: 'row',
+  width: '80%',
+  height: '4.125rem',
+  maxWidth: '25rem',
+  boxSizing: 'border-box',
+  border: '3px dashed #FE0303',
+  borderRadius: '16px',
+  backgroundColor: '#FFFFFFB2',
+  boxShadow: '0px 4px 14px 0px #00000040',
+});
 
 export const timerContent = style([
   flexCenterContainer,
