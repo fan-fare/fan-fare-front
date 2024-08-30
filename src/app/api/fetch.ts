3c43347ab89cc0d0084f0eacdf38c9d923404a61
sauce <dadad1004@naver.com>
2024-08-30 13:30:34 +0900

    feat: Add custom fetch function and modified it to return the body with the type of it

--- src/app/api/fetch.ts
+++ src/app/api/fetch.ts
@@ -1,3 +1,14 @@
+import { ICreateMessageRequest } from '@/interfaces/request';
+import {
+  ICheckIdResponse,
+  IReadMessageResponse,
+  ISignupResponse,
+} from '@/interfaces/response';
+
+// API base URL
+const apiBase = process.env.API_URL;
+
+// API URL paths
 const url = {
   isExisting: '/members/exist',
   signin: '/login',
@@ -8,26 +19,53 @@ const url = {
   deleteMessage: '/message',
 };
 
+/**
+ * Custom fetch function
+ * @param input url string
+ * @param init fetch options
+ * @returns promise of json response
+ */
+const customFetch = async <T = unknown>(
+  input: string | URL | globalThis.Request,
+  init?: RequestInit
+) => {
+  // create a new URL object with the input string
+  const url = new URL(input as string, apiBase);
+  const response = await fetch(url, {
+    ...init,
+    headers: {
+      ...init?.headers,
+      // set the content type to application/json
+      'Content-Type': 'application/json',
+    },
+  });
+  return response.json() as Promise<T>;
+};
+
+/**
+ * API functions
+ * @returns promise of json response
+ */
 export const api = {
   isExisting: async (id: BigInt) => {
-    return await fetch(`${url.isExisting}?id=${id}`, {
+    return await customFetch<ICheckIdResponse>(`${url.isExisting}?id=${id}`, {
       method: 'GET',
     });
   },
   login: async (data: FormData) => {
-    return await fetch(url.signin, {
+    return await customFetch<{}>(url.signin, {
       method: 'POST',
       body: data,
     });
   },
   signup: async (data: FormData) => {
-    return await fetch(url.signup, {
+    return await customFetch<ISignupResponse>(url.signup, {
       method: 'POST',
       body: data,
     });
   },
   getCake: async ({ memberId, page }: { memberId: BigInt; page: number }) => {
-    return await fetch(`${url.getCake}/${memberId}?page=${page}`, {
+    return await customFetch<{}>(`${url.getCake}/${memberId}?page=${page}`, {
       method: 'GET',
     });
   },
@@ -40,7 +78,7 @@ export const api = {
     message: string;
     nickname: string;
   }) => {
-    return await fetch('/api/post', {
+    return await customFetch<ICreateMessageRequest>('/api/post', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -49,12 +87,15 @@ export const api = {
     });
   },
   readMessage: async (messageId: BigInt) => {
-    return await fetch(`${url.readMessage}/${messageId}`, {
-      method: 'GET',
-    });
+    return await customFetch<IReadMessageResponse>(
+      `${url.readMessage}/${messageId}`,
+      {
+        method: 'GET',
+      }
+    );
   },
   deleteMessage: async (messageId: BigInt) => {
-    return await fetch(`${url.deleteMessage}/${messageId}`, {
+    return await customFetch<{}>(`${url.deleteMessage}/${messageId}`, {
       method: 'DELETE',
     });
   },
