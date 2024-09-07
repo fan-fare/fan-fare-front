336bac93a6d1785c5c3d8c45d67f968536efef4a
sauce-Git <dadad1004@naver.com>
2024-09-07 12:23:51 +0900

    feat: error display added

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,10 +1,25 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useErrorStore } from "@/store/error.store";
+import {
+  errorContainer,
+  errorText,
+  templateContainer,
+} from "@/styles/pages/template.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 export default function Template({ children }: { children: React.ReactNode }) {
+  const error = useErrorStore((state) => state);
+
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      {error.message && (
+        <div className={errorContainer}>
+          <div className={errorText}>{error.message}</div>
+        </div>
+      )}
+    </QueryClientProvider>
   );
 }
